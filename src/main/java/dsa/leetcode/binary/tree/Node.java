f640a1de0a9412f646513b271b9737f42a56c6ae
ved6053 <ved.cse@gmail.com>
2023-05-03 22:58:44 +0530

    LeetCode 04: SymmetricTree

---
+++ src/main/java/dsa/leetcode/binary/tree/Node.java
@@ -0,0 +1,12 @@
+package dsa.leetcode.binary.tree;
+
+public class Node {
+  public int data;
+  public Node left;
+
+  public Node right;
+
+  public Node(int data) {
+    this.data = data;
+  }
+}
