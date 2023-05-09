eefa1bafa59d3f98baac68fd08404b6702df2fb8
ved6053 <ved.cse@gmail.com>
2023-05-09 22:02:29 +0530

    LeetCode 08: SingleNumber - III- separate two number repeat exactly once.

--- src/main/java/dsa/leetcode/array/SingleNumber.java
+++ src/main/java/dsa/leetcode/array/SingleNumber.java
@@ -1,11 +1,28 @@
 package dsa.leetcode.array;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SingleNumber {
 
   public int findOddWithTwoRepeat(int[] arr) {
     int res = 0;
     for (int item : arr) {
       res ^= item;
+    }
+    return res;
+  }
+  public int[] findTwoOddWithTwoRepeat(int[] arr) {
+    int[] res = new int[2];
+    Map<Integer, Integer> map = new HashMap<>();
+    for(int num:arr){
+      map.put(num, map.getOrDefault(num, 0)+1);
+    }
+    int i=0;
+    for(Map.Entry<Integer, Integer> entry:map.entrySet()){
+      if(entry.getValue()==1){
+        res[i++] = entry.getKey();
+      }
     }
     return res;
   }
