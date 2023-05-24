b868096ba96642c2c1b67218127509e997bbfc09
ved6053 <ved.cse@gmail.com>
2023-05-24 21:22:49 +0530

    ContainerWithMostWater

--- src/main/java/dsa/leetcode/twopointer/ContainerWithMostWater.java
+++ src/main/java/dsa/leetcode/twopointer/ContainerWithMostWater.java
@@ -5,38 +5,38 @@ import java.security.InvalidAlgorithmParameterException;
 /** https://leetcode.com/problems/container-with-most-water/ */
 public class ContainerWithMostWater {
 
-  public int maxCapacityOfWaterContainer(int[] heightOfPillars)
+  public int maxCapacityOfWaterContainer( int[] heightOfWall)
       throws InvalidAlgorithmParameterException {
-    int numOfPillar = heightOfPillars.length;
-    int width = numOfPillar - 1;
-    if (width < 1) {
-      throw new InvalidAlgorithmParameterException("Number of pillar is less than two's");
-    }
-
-    int leftIndex = 0;
-    int rightIndex = numOfPillar - 1;
-    int result = 0;
-
-    while (width > 0) {
-      int leftPillar = heightOfPillars[leftIndex];
-      int rightPillar = heightOfPillars[rightIndex];
-
-      result = Math.max(result, width * Math.min(leftPillar, rightPillar));
-      boolean hasShiftLeftPillar = leftPillar <= rightPillar;
-      int minIndex = hasShiftLeftPillar ? leftIndex : rightIndex;
-      int minHeightPillar = hasShiftLeftPillar? leftPillar : rightPillar;
-      
-      while (width > 0 && heightOfPillars[minIndex] <= minHeightPillar) {
-        if (hasShiftLeftPillar) {
-          minIndex++;
-          leftIndex++;
-        } else {
-          minIndex--;
-          rightIndex--;
-        }
-        width--;
+      int numOfWall = heightOfWall.length;
+      int width = numOfWall - 1;
+      if (width < 1) {
+       throw new InvalidAlgorithmParameterException("Number of pillar is less than two's");
       }
-    }
-    return result;
+      
+      int leftIndex = 0;
+      int rightIndex = numOfWall - 1;
+      int result = 0;
+      
+      while (width > 0) {
+        int leftWall = heightOfWall[leftIndex];
+        int rightWall = heightOfWall[rightIndex];
+        
+        result = Math.max(result, width * Math.min(leftWall, rightWall));
+        boolean isShiftLeftWall = leftWall <= rightWall;
+        int minIndex = isShiftLeftWall ? leftIndex : rightIndex;
+        int minHeightPillar = isShiftLeftWall? leftWall : rightWall;
+        
+        while (width > 0 && heightOfWall[minIndex] <= minHeightPillar) {
+          if (isShiftLeftWall) {
+            minIndex++;
+            leftIndex++;
+          } else {
+            minIndex--;
+            rightIndex--;
+          }
+          width--;
+        }
+      }
+      return result;
   }
 }
