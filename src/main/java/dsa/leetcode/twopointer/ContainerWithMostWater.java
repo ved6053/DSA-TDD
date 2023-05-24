68c0425a81a70cde945f4af49b922c11e27bd675
ved6053 <ved.cse@gmail.com>
2023-05-24 21:17:07 +0530

    ContainerWithMostWater

--- src/main/java/dsa/leetcode/twopointer/ContainerWithMostWater.java
+++ src/main/java/dsa/leetcode/twopointer/ContainerWithMostWater.java
@@ -5,33 +5,34 @@ import java.security.InvalidAlgorithmParameterException;
 /** https://leetcode.com/problems/container-with-most-water/ */
 public class ContainerWithMostWater {
 
-  public int maxCapacityOfWaterContainer(int[] heightOfPillars) throws InvalidAlgorithmParameterException {
+  public int maxCapacityOfWaterContainer(int[] heightOfPillars)
+      throws InvalidAlgorithmParameterException {
     int numOfPillar = heightOfPillars.length;
     int width = numOfPillar - 1;
     if (width < 1) {
       throw new InvalidAlgorithmParameterException("Number of pillar is less than two's");
     }
-    
-    int leftMaxIndex = 0;
-    int rightMaxIndex = numOfPillar - 1;
+
+    int leftIndex = 0;
+    int rightIndex = numOfPillar - 1;
     int result = 0;
-    
-    while (width>0) {
-      int leftMaxHeightPillar = heightOfPillars[leftMaxIndex];
-      int rightMaxHeightPillar = heightOfPillars[rightMaxIndex];
+
+    while (width > 0) {
+      int leftPillar = heightOfPillars[leftIndex];
+      int rightPillar = heightOfPillars[rightIndex];
+
+      result = Math.max(result, width * Math.min(leftPillar, rightPillar));
+      boolean hasShiftLeftPillar = leftPillar <= rightPillar;
+      int minIndex = hasShiftLeftPillar ? leftIndex : rightIndex;
+      int minHeightPillar = hasShiftLeftPillar? leftPillar : rightPillar;
       
-      result = Math.max(result, width *  Math.min(leftMaxHeightPillar, rightMaxHeightPillar));
-      
-      int minIndex = leftMaxHeightPillar <rightMaxHeightPillar ? leftMaxIndex:rightMaxIndex;
-      int maxHeightPillar = leftMaxHeightPillar>rightMaxHeightPillar?leftMaxHeightPillar:rightMaxHeightPillar;
-      
-      while (width>0 && heightOfPillars[minIndex] <= maxHeightPillar) {
-        if(leftMaxHeightPillar!=maxHeightPillar) {
+      while (width > 0 && heightOfPillars[minIndex] <= minHeightPillar) {
+        if (hasShiftLeftPillar) {
           minIndex++;
-          leftMaxIndex++;
-        }else {
+          leftIndex++;
+        } else {
           minIndex--;
-          rightMaxIndex--;
+          rightIndex--;
         }
         width--;
       }
--- src/test/java/dsa/leetcode/twopointer/ContainerWithMostWaterTest.java
+++ src/test/java/dsa/leetcode/twopointer/ContainerWithMostWaterTest.java
@@ -8,27 +8,37 @@ import org.junit.jupiter.api.Test;
 
 class ContainerWithMostWaterTest {
 
-
   @Test
   /**
-   * Input: height = [1,8,6,2,5,4,8,3,7]
-   * Output: 49
-   * Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+   * Input: height = [1,8,6,2,5,4,8,3,7] Output: 49 Explanation: The above vertical lines are
+   * represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section)
+   * the container can contain is 49.
    */
   @Description("Test with valid input")
   void find_maxCapacityOfWaterContainer() throws InvalidAlgorithmParameterException {
-	  int[] heightOfPillars = {1,8,6,2,5,4,8,3,7};
-	  ContainerWithMostWater containerWithMostWater = new ContainerWithMostWater();
-	  assertEquals(49,containerWithMostWater.maxCapacityOfWaterContainer(heightOfPillars) );
+    int[] heightOfPillars = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+    ContainerWithMostWater containerWithMostWater = new ContainerWithMostWater();
+    assertEquals(49, containerWithMostWater.maxCapacityOfWaterContainer(heightOfPillars));
   }
-  
+
   @Test
   @Description("Edge case")
-  void  whenPillarSize_less_than_2_then_find_maxCapacityOfWaterContainer()   {
-	  int[] heightOfPillars = {1};
-	  ContainerWithMostWater containerWithMostWater = new ContainerWithMostWater();
-	  Throwable exception = assertThrows(InvalidAlgorithmParameterException.class, () ->containerWithMostWater.maxCapacityOfWaterContainer(heightOfPillars));
-	  assertEquals("Number of pillar is less than two's", exception.getMessage());
-	 
+  void whenPillarSize_less_than_2_then_find_maxCapacityOfWaterContainer() {
+    int[] heightOfPillars = {1};
+    ContainerWithMostWater containerWithMostWater = new ContainerWithMostWater();
+    Throwable exception =
+        assertThrows(
+            InvalidAlgorithmParameterException.class,
+            () -> containerWithMostWater.maxCapacityOfWaterContainer(heightOfPillars));
+    assertEquals("Number of pillar is less than two's", exception.getMessage());
+  }
+
+  @Test
+  void whenTestCase3_then_callMaxArea() throws InvalidAlgorithmParameterException {
+    int[] heightOfWalls = {1, 8, 6, 6, 2, 5, 9, 7, 8};
+    // width =8, 0 , 8 area= 8*1
+    //   1, 8 area= 7*8 = 56
+    ContainerWithMostWater containerWithMostWater = new ContainerWithMostWater();
+    assertEquals(56, containerWithMostWater.maxCapacityOfWaterContainer(heightOfWalls));
   }
 }
