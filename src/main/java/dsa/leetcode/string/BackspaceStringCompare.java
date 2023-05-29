c664203a89ce2cec2570a308b6f3f9c2a984ddcf
ved6053 <ved.cse@gmail.com>
2023-05-29 20:48:11 +0530

    844. Backspace String Compare - O(1) SPACE

--- src/main/java/dsa/leetcode/string/BackspaceStringCompare.java
+++ src/main/java/dsa/leetcode/string/BackspaceStringCompare.java
@@ -5,12 +5,43 @@ import java.util.Stack;
 /**
  * https://leetcode.com/problems/backspace-string-compare/
  * https://www.youtube.com/watch?v=TfENDUMm0XE&list=PL1w8k37X_6L86f3PUUVFoGYXvZiZHde1S&index=14
+ * https://www.youtube.com/watch?v=tUxW1JwEb9M&list=PL1w8k37X_6L86f3PUUVFoGYXvZiZHde1S&index=15
  **/
 public class BackspaceStringCompare {
 	public Boolean backspaceCompare(String str1, String str2){
-		String result1 = removeBackspace(str1);
-		String result2 = removeBackspace(str2);
-		return result1.equals(result2);
+		char [] s1 = str1.toCharArray();
+		char [] s2 = str2.toCharArray();
+		int i = s1.length-1, j = s2.length-1;
+		while(i>=0 || j>=0){
+			int count=0;
+			while(i>=0 && (count>0||s1[i]=='#')){
+				if(s1[i]=='#')
+					count++;
+				else
+					count--;
+				i--;
+			}
+			while(j>=0 && (count>0||s2[j]=='#')){
+				if(s2[j]=='#')
+					count++;
+				else
+					count--;
+				j--;
+			}
+			if(i>=0&&j>=0){
+				if(s1[i]!=s2[j])
+					return false;
+				else {
+					i--;
+					j--;
+				}
+			} else if(i>=0||j>=0){
+				return false;
+			}
+			
+		}
+		
+		return true;
 	}
 	
 	private String removeBackspace(String str){
--- src/test/java/dsa/leetcode/string/BackspaceStringCompareTest.java
+++ src/test/java/dsa/leetcode/string/BackspaceStringCompareTest.java
@@ -6,43 +6,41 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class BackspaceStringCompareTest {
-	private  BackspaceStringCompare backspaceStringCompare;
-	@BeforeEach
-	void setBackspaceStringCompare() {
-		backspaceStringCompare = new BackspaceStringCompare();
-	}
+  private BackspaceStringCompare backspaceStringCompare;
 
- 
-  /**
-   * Input: s = "ab#c", t = "ad#c"
-   * Output: true
-   * Explanation: Both s and t become "ac".
-   **/
+  @BeforeEach
+  void setBackspaceStringCompare() {
+    backspaceStringCompare = new BackspaceStringCompare();
+  }
+
+  /** Input: s = "ab#c", t = "ad#c" Output: true Explanation: Both s and t become "ac". */
   @Test
   void whenTestCase1_then_callBackspaceStringCompare() {
-	  String  s = "ab#c", t = "ad#c";
-	  assertTrue(backspaceStringCompare.backspaceCompare(s, t));
-	}
+    String s = "ab#c", t = "ad#c";
+    assertTrue(backspaceStringCompare.backspaceCompare(s, t));
+  }
 
-  /**
-   * Input: s = "ab##", t = "c#d#"
-   * Output: true
-   * Explanation: Both s and t become "".
-   **/
+  /** Input: s = "ab##", t = "c#d#" Output: true Explanation: Both s and t become "". */
   @Test
   void whenTestCase2_then_callBackspaceStringCompare() {
-	  String  s = "ab##", t = "c#d#";
-	  assertTrue(backspaceStringCompare.backspaceCompare(s, t));
+    String s = "ab##", t = "c#d#";
+    assertTrue(backspaceStringCompare.backspaceCompare(s, t));
+  }
+
+  /** Input: s = "a#c", t = "b" Output: false Explanation: s becomes "c" while t becomes "b". */
+  @Test
+  void whenTestCase3_then_callBackspaceStringCompare() {
+    String s = "a#c", t = "b";
+    assertFalse(backspaceStringCompare.backspaceCompare(s, t));
   }
 
   /**
-   * Input: s = "a#c", t = "b"
-   * Output: false
-   * Explanation: s becomes "c" while t becomes "b".
-   **/
+   * Input: s = "nzp#om##g", t = "b#nzp#op##g" Output: true Explanation: s becomes "nzg" while t
+   * becomes "nzg".
+   */
   @Test
-  void whenTestCase3_then_callBackspaceStringCompare() {
-	  String s = "a#c", t = "b";
-	  assertFalse(backspaceStringCompare.backspaceCompare(s, t));
+  void whenTestCase4_then_callBackspaceStringCompare() {
+    String s = "nzp#om##g", t = "ab##nzp#op##g";
+    assertTrue(backspaceStringCompare.backspaceCompare(s, t));
   }
 }
