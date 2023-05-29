c00cec6a9af1846189c46c9d96b1e9cfd3254b78
ved6053 <ved.cse@gmail.com>
2023-05-29 20:52:57 +0530

    Interview

---
+++ src/main/java/interview/Coforge2.java
@@ -0,0 +1,55 @@
+package interview;
+
+public class Coforge2 {
+/**
+*  max amount of water contian with between wall
+ * same , area wall is negilible
+ * vol dis front + back =fix area height*dis=  area;
+ *
+ * |
+ * |   |  |    |
+ * |   |  |  | |
+ * |   |  |  | |
+ * height[] ={1, 8, 6, 6, 2, 5, 9, 7, 8}
+ *            leftIndex
+ *            rightIndex
+ * maxium area
+ * width = 8
+ * area=8*1= 8
+ * area = 6*5= 30
+*/
+    int maxArea(int[] heightOfWall){
+	    int numOfWall = heightOfWall.length;
+	    int width = numOfWall - 1;
+	    if (width < 1) {
+		    return  -1;
+	    }
+	    
+	    int leftIndex = 0;
+	    int rightIndex = numOfWall - 1;
+	    int result = 0;
+	    
+	    while (width > 0) {
+		    int leftWall = heightOfWall[leftIndex];
+		    int rightWall = heightOfWall[rightIndex];
+		    
+		    result = Math.max(result, width * Math.min(leftWall, rightWall));
+		    boolean isShiftLeftWall = leftWall <= rightWall;
+		    int minIndex = isShiftLeftWall ? leftIndex : rightIndex;
+		    int minHeightPillar = isShiftLeftWall? leftWall : rightWall;
+		    
+		    while (width > 0 && heightOfWall[minIndex] <= minHeightPillar) {
+			    if (isShiftLeftWall) {
+				    minIndex++;
+				    leftIndex++;
+			    } else {
+				    minIndex--;
+				    rightIndex--;
+			    }
+			    width--;
+		    }
+	    }
+	    return result;
+    }
+
+}
---
+++ src/test/java/Test05Test.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import interview.Coforge2;
+import org.junit.jupiter.api.Test;
+
+class Test05Test {
+   private final Coforge2 test= new Coforge2();
+   
+   @Test
+   void maxArea(){
+	  int[] heightOfWalls ={1, 8, 6, 6, 2, 5, 9, 7, 8};
+	  //width =8, 0 , 8 area= 8*1
+	  //   1, 8 area= 7*8 = 56
+	  assertEquals(56, test.maxArea(heightOfWalls));
+   }
+}
