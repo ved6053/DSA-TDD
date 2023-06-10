80bef61d63821fcce4c48634d55ae48f74263d07
ved6053 <ved.cse@gmail.com>
2023-06-10 20:48:22 +0530

    https://www.algoexpert.io/questions/two-number-sum time complexity O(nlogn) and space complexity O(1)

--- src/main/java/dsa/algoexpert/hashing/P001TwoNumberSum.java
+++ src/main/java/dsa/algoexpert/hashing/P001TwoNumberSum.java
@@ -1,24 +1,25 @@
 package dsa.algoexpert.hashing;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Arrays;
 
-/**
- * https://www.algoexpert.io/questions/two-number-sum
- **/
+/** https://www.algoexpert.io/questions/two-number-sum */
 public class P001TwoNumberSum {
 
-  //time complexity O(n) and space complexity O(n)
-  public  int[] twoNumberSum(int[] array, int targetSum) {
-    Set<Integer> hashSet = new HashSet<>();
-    for (int element : array) {
-      if (hashSet.contains(targetSum - element)) {
-        return new int[] {targetSum - element, element};
+  // time complexity O(nlogn) and space complexity O(1)
+  public int[] twoNumberSum(int[] array, int targetSum) {
+    Arrays.sort(array);
+    int left = 0, right = array.length - 1;
+    while (left < right) {
+      int currentSum = array[left] + array[right];
+      if (currentSum == targetSum) {
+        return new int[] {array[left], array[right]};
+      } else if (currentSum < targetSum) {
+        left += 1;
+      } else if (currentSum > targetSum) {
+        right -= 1;
       }
-      hashSet.add(element);
     }
     // Write your code here.
     return new int[0];
   }
-  
 }
--- src/test/java/dsa/algoexpert/hashing/P001TwoNumberSumTest.java
+++ src/test/java/dsa/algoexpert/hashing/P001TwoNumberSumTest.java
@@ -2,6 +2,7 @@ package dsa.algoexpert.hashing;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 class P001TwoNumberSumTest {
@@ -11,103 +12,84 @@ class P001TwoNumberSumTest {
   void testCase1() {
     int[] array = {3, 5, -4, 8, 11, 1, -1, 6};
 	int targetSum = 10;
-	int[] res = obj.twoNumberSum(array, targetSum);
-    assertEquals(11, res[0]);
-    assertEquals(-1, res[1]);
+	Arrays.equals(new int[]{11, -1}, obj.twoNumberSum(array, targetSum));
+  
   }
 
   @Test
   void testCase2() {
       int[] array = {4, 6};
 	  int targetSum = 10;
-	  int[] res = obj.twoNumberSum(array, targetSum);
-	  assertEquals(4, res[0]);
-	  assertEquals(6, res[1]);
+	  Arrays.equals(new int[]{4, 6}, obj.twoNumberSum(array, targetSum));
   }
 
   @Test
   void testCase3() {
     int[] array = {4, 6, 1};
 	int targetSum = 5;
-	int[] res = obj.twoNumberSum(array, targetSum);
-	assertEquals(4, res[0]);
-	assertEquals(1, res[1]);
+	Arrays.equals(new int[]{4, 1}, obj.twoNumberSum(array, targetSum));
   }
 
   @Test
   void testCase4() {
     int[] array = {4, 6, 1, -3};
 	int targetSum = 3;
-	int[] res = obj.twoNumberSum(array, targetSum);
-	assertEquals(6, res[0]);
-	assertEquals(-3, res[1]);
+	Arrays.equals(new int[]{6, -3}, obj.twoNumberSum(array, targetSum));
   }
 
   @Test
   void testCase5() {
     int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 	int targetSum = 17;
-	int[] res = obj.twoNumberSum(array, targetSum);
-	assertEquals(8, res[0]);
-	assertEquals(9, res[1]);
+	Arrays.equals(new int[]{8, 9}, obj.twoNumberSum(array, targetSum));
   }
 
   @Test
   void testCase6() {
     int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 15};
 	int targetSum = 18;
-	int[] res = obj.twoNumberSum(array, targetSum);
-	assertEquals(3, res[0]);
-	assertEquals(15, res[1]);
+	Arrays.equals(new int[]{3, 15}, obj.twoNumberSum(array, targetSum));
   }
 
   @Test
   void testCase7() {
     int[] array = {-7, -5, -3, -1, 0, 1, 3, 5, 7};
 	int targetSum = -5;
-	int[] res = obj.twoNumberSum(array, targetSum);
-	assertEquals(-5, res[0]);
-	assertEquals(0, res[1]);
+	Arrays.equals(new int[]{-5, 0}, obj.twoNumberSum(array, targetSum));
   }
 
   @Test
   void testCase8() {
     int[] array = {-21, 301, 12, 4, 65, 56, 210, 356, 9, -47};
 	int targetSum = 163;
-	int[] res = obj.twoNumberSum(array, targetSum);
-	assertEquals(210, res[0]);
-	assertEquals(-47, res[1]);
+	Arrays.equals(new int[]{210, -47}, obj.twoNumberSum(array, targetSum));
   }
 
   @Test
   void testCase9() {
     int[] array = {-21, 301, 12, 4, 65, 56, 210, 356, 9, -47};
 	int targetSum = 164;
-	int[] res = obj.twoNumberSum(array, targetSum);
-	assertEquals(0, res.length);
+	Arrays.equals(new int[0], obj.twoNumberSum(array, targetSum));
   }
 
   @Test
   void testCase10() {
     int[] array = {3, 5, -4, 8, 11, 1, -1, 6};
 	int targetSum = 15;
-	int[] res = obj.twoNumberSum(array, targetSum);
-	assertEquals(0, res.length);
+	Arrays.equals(new int[0], obj.twoNumberSum(array, targetSum));
   }
 
   @Test
   void testCase11() {
     int[] array = {14};
 	int targetSum = 15;
-	int[] res = obj.twoNumberSum(array, targetSum);
-	assertEquals(0, res.length);
+	Arrays.equals(new int[0], obj.twoNumberSum(array, targetSum));
   }
 
   @Test
   void testCase12() {
     int[] array = {15};
 	int targetSum = 15;
-	int[] res = obj.twoNumberSum(array, targetSum);
-	assertEquals(0, res.length);
+	Arrays.equals(new int[0], obj.twoNumberSum(array, targetSum));
   }
 }
