1559282605f27a415370367274c5a6ca7198a6fe
ved6053 <ved.cse@gmail.com>
2023-06-10 21:01:54 +0530

    https://www.algoexpert.io/questions/sorted-squared-array Soultion in attempt2-hint

--- src/main/java/dsa/algoexpert/array/P003SortedSquaredArray.java
+++ src/main/java/dsa/algoexpert/array/P003SortedSquaredArray.java
@@ -3,42 +3,19 @@ package dsa.algoexpert.array;
 public class P003SortedSquaredArray {
 public int[] sortedSquaredArray(int[] array) {
 	int[] res = new int[array.length];
-	int mergePoint=-1;
-	for(int i=0;i<array.length;i++){
-		if(array[i]<0){
-			mergePoint++;
-		} else{
-			break;
-		}
-	}
-	
-	int left=mergePoint, right=mergePoint+1;
-	int index=0;
-	while(left>=0&&right<array.length) {
+	int left=0, right=array.length-1;
+	int end=array.length-1;
+	while(left<=right){
 		int leftValue=array[left]*array[left];
 		int rightValue=array[right]*array[right];
-		if(leftValue==rightValue){
-			res[index++]=leftValue;
-			res[index++]=rightValue;
-			left--;
-			right++;
-		} else if(leftValue<rightValue){
-			res[index++]=leftValue;
-			left--;
-		} else if(leftValue>rightValue){
-			res[index++]=rightValue;
-			right++;
+		if(leftValue>rightValue){
+			res[end--]=leftValue;
+			left++;
+		} else{
+			res[end--]=rightValue;
+			right--;
 		}
 	}
-	while(left>=0){
-		res[index++]=array[left]*array[left];
-		left--;
-	}
-	while(right<array.length){
-		res[index++]=array[right]*array[right];
-		right++;
-	}
-	
 	return res;
 }
 }
