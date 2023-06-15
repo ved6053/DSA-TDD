6c390580214306bcd66555a43590390d2371029c
ved6053 <ved.cse@gmail.com>
2023-06-15 15:38:46 +0530

    https://leetcode.com/problems/min-stack/ https://www.youtube.com/watch?v=SM8-Z99SJ_I&list=PL1w8k37X_6L86f3PUUVFoGYXvZiZHde1S&index=16

--- src/main/java/dsa/leetcode/stack/MinStackCommand.java
+++ src/main/java/dsa/leetcode/stack/MinStackCommand.java
@@ -1,5 +1,38 @@
 package dsa.leetcode.stack;
 
+import java.util.Stack;
+
+/**
+ * https://leetcode.com/problems/min-stack/
+ * https://www.youtube.com/watch?v=SM8-Z99SJ_I&list=PL1w8k37X_6L86f3PUUVFoGYXvZiZHde1S&index=16
+ **/
+class MinStack {
+	private final Stack<Integer> stack;
+	private final Stack<Integer> sideStack;
+	public MinStack() {
+		stack= new Stack<>();
+		sideStack = new Stack<>();
+	}
+	
+	public void push(int val) {
+		stack.push(val);
+		sideStack.push((sideStack.isEmpty()||sideStack.peek()>val)?val:sideStack.peek());
+	}
+	
+	public int pop() {
+		sideStack.pop();
+		return stack.pop();
+	}
+	
+	public int peek() {
+		return stack.peek();
+	}
+	
+	public int getMin() {
+		return sideStack.peek();
+	}
+
+}
 
 public class MinStackCommand {
 	private MinStack minStack;
